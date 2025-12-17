e2423c823e3638a879b709e12aaff679d4a1110b
maksbernatskyy <maksbernatskyy@gmail.com>
2025-12-17 15:36:00 +0100

    Update route for DetailTravel page to include dynamic ID parameter

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
        <Routes>
         <Route element={<DefaultLayout />}>
           <Route path="/" element={<HomePage />}/>
-          <Route path="/travel" element={<DetailTravel />}/>
+          <Route path="/detail/:id" element={<DetailTravel />}/>
         </Route>
        </Routes>
      </BrowserRouter>
