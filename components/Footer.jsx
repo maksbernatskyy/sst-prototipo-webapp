8509ca8660c73f61dfd0004cd00afbc4eae94f6f
maksbernatskyy <maksbernatskyy@gmail.com>
2025-12-17 17:02:41 +0100

    Add NavBar component and refactor Header and Footer components

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,7 @@
 export default function Footer() {
     return (
-        <>
         <footer>
-            
+            <p>© 2025 - My Travel App</p>
         </footer>
-        </>
-    )
+    );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,9 @@
+import NavBar from "./NavBar";
+
 export default function Header() {
-    return (
-        <>
-        <header>
-            
-        </header>
-        </>
-    )
+  return (
+    <header>
+      <NavBar />
+    </header>
+  );
 }
