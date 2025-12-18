b4eb028054cb544174b75897139094b45d1a753f
maksbernatskyy <maksbernatskyy@gmail.com>
2025-12-18 12:12:38 +0100

    Fix DetailTravel component to include index parameter in user mapping

--- src/pages/DetailTravel.jsx
+++ src/pages/DetailTravel.jsx
@@ -16,7 +16,7 @@ export default function DetailTravel() {
         </h1>
 
         <div className="accordion" id="accordionExample">
-          {travelUsers.map((user) => (
+          {travelUsers.map((user, i) => (
             <div key={user.id} className="accordion-item">
               <h2 className="accordion-header">
                 <button
